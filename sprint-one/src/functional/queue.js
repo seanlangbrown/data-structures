21ef3023f7711d73cf9c388b86ad4abfd132a189
Seanlangbrown <seanlangbrown@gmail.com>
2018-01-17 11:50:06 -0800

    completed functional/queue.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var last = 0
+  var next = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[last] = value;
+    last++;
   };
 
   someInstance.dequeue = function() {
+    if(last > next) {
+      var result = storage[next];
+      delete storage[next]; //memory management
+      next++;
+      return result;
+    }
   };
 
   someInstance.size = function() {
+    return last - next;
   };
 
   return someInstance;
