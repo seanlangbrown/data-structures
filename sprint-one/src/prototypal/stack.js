201ee9f34773d427d3dba77fc51a03393cecea9d
Stacy Lin <stayclin@gmail.com>
2018-01-17 15:11:56 -0800

    passed prototypal stack tests

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = Object.create(stackMethods);
+  someInstance.count = 0;
+  someInstance.storage = {};
+
+
+  return someInstance;
 };
 
-var stackMethods = {};
+
+var stackMethods = {
+  push: function(value) {
+    this.count += 1;
+    this.storage[this.count] = value;
+  },
+
+  pop: function() {
+    var val = this.storage[this.count];
+    delete this.storage[this.count];
+    this.count -= 1;
+    return val;
+  },
+
+  size: function() {
+    if(this.count < 0){
+      return 0;
+    }
+    return this.count;
+  }
+
+};
 
 
