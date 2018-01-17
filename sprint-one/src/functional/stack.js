d355688a1d83fbe9c2400c95b34defbecfcc2e70
Stacy Lin <stayclin@gmail.com>
2018-01-17 11:20:35 -0800

    functional pattern stack shared behavior passed till sequentially adding and removing items

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,21 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    count += 1;
   };
 
   someInstance.pop = function() {
+    count -= 1;
   };
 
   someInstance.size = function() {
+    if(count < 0){
+      return 0;
+    }
+    return count;
   };
 
   return someInstance;
