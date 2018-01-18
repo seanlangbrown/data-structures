6644d03feaf2d4c05e94a7f678888bea8dbfa3d6
seanlangbrown <seanlangbrown@gmail.com>
2018-01-17 20:18:30 -0800

    Tests passed till removeHead

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -6,12 +6,44 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //create new node
+    var newNode = Node(value);
+    //if there is no head
+    if (!this.head) {
+      //point head and tail to new node
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    //if there is a head
+    //point current tail node to new node
+    this.tail.next = newNode;
+    //point tail to new node
+    this.tail = newNode;
+      
   };
 
   list.removeHead = function() {
+    var val = this.head.value;
+    this.head = this.head.next;
+    return val;
   };
 
   list.contains = function(target) {
+    //first input=this.head
+    var nodeSearcher = function(node) {
+      //check if value = target
+      if (node.value === target) {
+        return true;
+      } else if (!node.next) {
+        return false;
+      } else { 
+        return nodeSearcher(node.next);
+      }
+      //if true, return true
+      //else if nextNode is null, return false
+      //else, call self on nextnode
+    };
+    
   };
 
   return list;
