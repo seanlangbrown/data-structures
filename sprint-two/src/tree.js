10e5133cf0e6548f73dfd01d37348c85cc81fddd
seanlangbrown <seanlangbrown@gmail.com>
2018-01-17 20:56:45 -0800

    completed tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(treeMethods);//{};
   newTree.value = value;
+  //_.extend(newTree, treeMethods);
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,9 +12,29 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //create a new tree with value and push to the array of children
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  //input: the node that contains is called on (this)
+  //if this nodes value is the target, return true
+  if (this.value === target) {
+    return true;
+  } else if (!this.children.length) {
+    //if this node has no children
+    return false;
+  } else {
+    //if this node has children
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        //if found in any child, stop search and return true
+        return true;
+      }
+    }
+    return false;
+  }
+
 };
 
 
