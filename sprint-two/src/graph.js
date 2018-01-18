e0a16238889fdb1b87e50c75637345e570f4c158
seanlangbrown <seanlangbrown@gmail.com>
2018-01-17 21:38:28 -0800

    partially completed graph.js till remove

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,18 +2,29 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+//inserts a node in nodes as node: {}
+  this.nodes[node] = {};
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  //check if node is in nodes{}
+  if (this.nodes[node]) {
+    return true;
+  }
+  return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  //remove the node from nodes
+  delete this.nodes[node];
+
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
@@ -36,4 +47,7 @@ Graph.prototype.forEachNode = function(cb) {
  * Complexity: What is the time complexity of the above functions?
  */
 
-
+/*var Node = function() {
+  this.value;
+  this.edges = [];
+};*/
