e345b1e51c01f057a823bcab4d5b7d06a4f15cca
seanlangbrown <seanlangbrown@gmail.com>
2018-01-19 20:11:50 -0800

    Added implementation for BreadthFirstLog

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,7 @@
 var BinarySearchTree = function(value) {
  
-  this.left;
-  this.right;
+  this.left = null;
+  this.right = null;
   this.value = value;
 
 };
@@ -19,7 +19,7 @@ BinarySearchTree.prototype.insert = function(value) {
     //check if there is a left child
     //if no child, append as left child
     //call insert on left child
-    if (this.left === undefined) {
+    if (!this.left) {
       this.left = newNode;
     } else {
       this.left.insert(value);
@@ -27,7 +27,7 @@ BinarySearchTree.prototype.insert = function(value) {
   } else {
     //if the new value is greater than the current node value, go to the right
     //do same stuff but to the right child
-    if (this.right === undefined) {
+    if (!this.right) {
       this.right = new BinarySearchTree(value);
     } else {
       this.right.insert(value);
@@ -49,13 +49,13 @@ BinarySearchTree.prototype.contains = function(value) {
   //check if value < current node 
   if (value < this.value) {
     //if true: check for left child
-    if (this.left !== undefined) {
+    if (this.left) {
       //if LC exists return call contains on left child
       return this.left.contains(value);
     }
   } else {
     //if false: check for right child
-    if (this.right !== undefined) {
+    if (this.right) {
       // if RC exists return call contains on right child
       return this.right.contains(value);
     }
@@ -65,21 +65,48 @@ BinarySearchTree.prototype.contains = function(value) {
 };
 
 BinarySearchTree.prototype.depthFirstLog = function(cb) {
-  //search through all nodes (left child first) and pass value into the callback function
+  //search through all nodes - depth first (left child first) and pass value into the callback function
   //call the callback, pass in value
   cb(this.value);
 
   //call depthFirstLog on left child
-  if (this.left !== undefined) {
+  if (this.left) {
     this.left.depthFirstLog(cb);
   }
   //call depthFirstLog on right child
-  if (this.right !== undefined) {
+  if (this.right) {
     this.right.depthFirstLog(cb);
   }
 };
 
 
+BinarySearchTree.prototype.breadthFirstLog = function(cb, nodes) {
+  //search through all nodes - breadth first (left child first) and pass value into the callback function
+  //call the callback, pass in value
+  
+  //base case, top node, no array passed in
+  //recursive case, nodes are all the nodes at one level
+  nodes = nodes || [this];
+  
+  var nextNodes = []; //to store the children in the next level
+  //for each element in the input array
+  for (var i = 0; i < nodes.length; i++) {
+    //call cb on the nodes,
+    cb(nodes[i].value);
+    //push left child, then right child to next Nodes
+    if (nodes[i].left) {
+      nextNodes.push(nodes[i].left);
+    }
+    if (nodes[i].right) {
+      nextNodes.push(nodes[i].right);
+    }
+  }
+
+  //call recursively on array of all children
+  if (nextNodes.length){
+    this.breadthFirstLog(cb, nextNodes);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
